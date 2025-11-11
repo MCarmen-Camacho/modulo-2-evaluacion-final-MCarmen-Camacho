15b0794f1721902604dae74efba0da5a9b8ec9d2
MCarmen-Camacho <mcarmen.camacho@outlook.es>
2025-11-11 18:28:48 +0100

    Update layout

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 "use strict";
 
 //QUERY.SELECTOR
-
+const searchInput = document.querySelector(".js-search-input");
+const searchBtn = document.querySelector(".js-search-btn");
+const productsSection = document.querySelector(".js-products");
 
 //FUNCIONES
 
-
 //EVENTOS
-
 
 //CÓDIGO QUE SE LANZA CUANDO SE CARGA LA PÁGINA
