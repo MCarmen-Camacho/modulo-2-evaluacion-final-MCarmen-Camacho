4625691168e09c170589dced3a56b2597a8d5bc8
MCarmen-Camacho <mcarmen.camacho@outlook.es>
2025-11-12 13:15:12 +0100

    Add functionality to add products to the cart

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@
 const searchInput = document.querySelector(".js-search-input");
 const searchBtn = document.querySelector(".js-search-btn");
 const productsSection = document.querySelector(".js-products");
+const cartList = document.querySelector(".js-cart-list");
 
 // VARIABLES GLOBALES
-
 let products = [];
+let cart = [];
 
 // FUNCIONES
 
@@ -36,12 +37,45 @@ function renderProducts(productList) {
         <img class="product__img" src="${image}" alt="${product.title}">
         <h3 class="product__title">${product.title}</h3>
         <p class="product__price">${product.price.toFixed(2)} €</p>
-        <button class="product__btn">Comprar</button>
+        <button class="product__btn js-add-btn" data-id="${
+          product.id
+        }">Comprar</button>
       </article>
     `;
   }
 
   productsSection.innerHTML = html;
+
+  const addButtons = document.querySelectorAll(".js-add-btn");
+  for (const button of addButtons) {
+    button.addEventListener("click", handleAddToCart);
+  }
+}
+
+function renderCart() {
+  let html = "";
+
+  if (cart.length === 0) {
+    html = `<li class="cart__item--empty">Tu carrito está vacío</li>`;
+  } else {
+    for (const item of cart) {
+      html += `<li class="cart__item">${item.title} - ${item.price.toFixed(
+        2
+      )} €</li>`;
+    }
+  }
+
+  cartList.innerHTML = html;
+}
+
+function handleAddToCart(event) {
+  const productId = parseInt(event.currentTarget.dataset.id);
+  const selectedProduct = products.find((product) => product.id === productId);
+
+  if (selectedProduct) {
+    cart.push(selectedProduct);
+    renderCart();
+  }
 }
 
 function handleSearch() {
@@ -54,9 +88,8 @@ function handleSearch() {
 }
 
 // EVENTOS
-
 searchBtn.addEventListener("click", handleSearch);
 
 // CÓDIGO AL CARGAR LA PÁGINA
-
 getProductsFromAPI();
+renderCart();
